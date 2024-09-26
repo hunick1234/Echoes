a54c8d3bc0c2f580d9270edb73786caa3fe74b02
nick <hunick1234@gmail.com>
2024-09-26 11:34:29 +0800

    feat: complete registration feature

--- actor/handler/handler.go
+++ actor/handler/handler.go
@@ -1,42 +1,66 @@
 package handler
 
 import (
+	"context"
 	"net/http"
+	"time"
 
+	"github.com/hunick1234/Echoes/actor/model"
 	"github.com/hunick1234/Echoes/actor/repository"
 	"github.com/hunick1234/Echoes/actor/service"
 	httpb "github.com/hunick1234/Echoes/server/httpB"
 )
 
-type ActorHandler struct {
-	Service service.ActorService
-	res     http.ResponseWriter
-	req     *http.Request
+func StartUserHandle(router *httpb.WrappedMux) {
+	repo := repository.DefaultActorRepo()
+	srv := service.NewActorService(repo)
+	h := httpb.NewServiceHandler(srv, nil, nil)
+
+	router.Handle("/", RegisterPage("/"))
+	router.Get("/register", httpb.Handler(Register, h))
+	router.Get("/resend-mail", httpb.Handler(Resend, h))
 }
 
-func NewUserHandler(srv service.ActorService, res http.ResponseWriter, req *http.Request) ActorHandler {
-	return ActorHandler{
-		Service: srv,
-		res:     res,
-		req:     req,
+func RegisterPage(prefix string) http.Handler {
+	staticDir := http.Dir("../../web/actor")
+	fileServer := http.FileServer(staticDir)
+	return http.StripPrefix(prefix, fileServer)
+}
+
+func Register(u *httpb.ServiceHandler[service.ActorService]) error {
+	mail := u.GetReq().URL.Query().Get("mail")
+
+	//check then register
+	if ok := u.GetSrv().CheckByMail(mail); !ok {
+		registerActor := &model.RegisterActor{
+			Mail:   mail,
+			MailId: model.GenerateID(mail),
+		}
+		err := u.GetSrv().CreatUser(registerActor)
+		if err != nil {
+			return err
+		}
 	}
-}
 
-func StartUserHAndle(router *httpb.WrappedMux) {
-	router.Get("/register", handler(Register))
-}
+	ctx, cancel := context.WithTimeout(u.GetReq().Context(), 2*time.Second)
+	defer cancel()
 
-func handler(api func(*ActorHandler)) http.HandlerFunc {
-	return func(res http.ResponseWriter, req *http.Request) {
-		repo := repository.NewActorRepo()
-		srv := service.NewActorService(&repo)
-		u := NewUserHandler(srv, res, req)
-		api(&u)
-
+	sendMailUrl := "http://127.0.0.1:5050/send-register-mail?" + mail
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, sendMailUrl, nil)
+	if err != nil {
+		return err
 	}
+
+	// 發送 HTTP 請求
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
 }
 
-func Register(u *ActorHandler) {
-	u.Register()
-	http.Get("http://127.0.0.1:5050/SendRegisterMail")
+func Resend(a *httpb.ServiceHandler[service.ActorService]) error {
+	return nil
 }
